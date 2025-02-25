a071aa82f5a6317b4728a92e2919424eae66d277
Wojtek Wasko <wojciechwasko@gmail.com>
2025-02-25 19:39:44 +0100

    Allow Windows to shut us down gracefully, use auto-off feature

--- webcam.ps1
+++ webcam.ps1
@@ -22,49 +22,65 @@ $regMaxDepth = 5
 $user = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
 $regBasePath = "Registry::HKEY_USERS\$user\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam"
 
-function Toggle-KasaDevice {
-    param ([bool]$turnOn)
-    $action = if ($turnOn) { "on" } else { "off" }
-	& $kasaBinary --username $username --password $password --host $kasaDeviceIP --type $deviceType $action
+function KasaDeviceDo {
+    param ([string[]]$action)
+    & $kasaBinary --username $username --password $password --host $kasaDeviceIP --type $deviceType @action
 }
 
 function Scan-WebcamRegistry {
-    param ([string]$path, [int]$depth)
+	param ([string]$path, [int]$depth)
 
-    if ($depth -gt $regMaxDepth) { return $false }
-    if (-not (Test-Path $path)) { return $false }
-    
-    try {
-        $subKeys = Get-ChildItem -Path $path -ErrorAction Continue
+	if ($depth -gt $regMaxDepth) { return $false }
+	if (-not (Test-Path $path)) { return $false }
+	
+	try {
+		$subKeys = Get-ChildItem -Path $path -ErrorAction Continue
 
-        foreach ($subKey in $subKeys) {
-            $webcamUsage = Get-ItemProperty -Path $subKey.PSPath -ErrorAction Continue
-            
-            if ($webcamUsage) {
-                $startTimeRaw = $webcamUsage.PSObject.Properties["LastUsedTimeStart"].Value
-                $stopTimeRaw = $webcamUsage.PSObject.Properties["LastUsedTimeStop"].Value
-                
-                # Webcam is in use if Start > 0 and Stop is 0
-                if ($startTimeRaw -gt 0 -and $stopTimeRaw -eq 0) {
-                    return $true
-                }
-            }
+		foreach ($subKey in $subKeys) {
+			$webcamUsage = Get-ItemProperty -Path $subKey.PSPath -ErrorAction Continue
+			
+			if ($webcamUsage) {
+				$startTimeRaw = $webcamUsage.PSObject.Properties["LastUsedTimeStart"].Value
+				$stopTimeRaw = $webcamUsage.PSObject.Properties["LastUsedTimeStop"].Value
+				
+				# Webcam is in use if Start > 0 and Stop is 0
+				if ($startTimeRaw -gt 0 -and $stopTimeRaw -eq 0) {
+					return $true
+				}
+			}
 
-            if (Scan-WebcamRegistry -path $subKey.PSPath -depth ($depth + 1)) {
-                return $true
-            }
-        }
-    } catch {
-        Write-Output "Error reading registry: $_"
-    }
-    return $false
+			if (Scan-WebcamRegistry -path $subKey.PSPath -depth ($depth + 1)) {
+				return $true
+			}
+		}
+	} catch {
+		Write-Output "Error reading registry: $_"
+	}
+	return $false
 }
 
-while ($true) {
-    if (Scan-WebcamRegistry -path $regBasePath -depth 0) {
-        Toggle-KasaDevice -turnOn $true
-    } else {
-        Toggle-KasaDevice -turnOn $false
-    }
+$stopScript = $false
+
+# Catch termination signal
+$Handler = {
+    $global:stopScript = $true
+}
+Register-EngineEvent -SourceIdentifier "PowerShell.Exiting" -Action $Handler
+
+
+KasaDeviceDo -action @("feature", "auto_off_minutes", "1")
+while (-not $stopScript) {
+	if (Scan-WebcamRegistry -path $regBasePath -depth 0) {
+		KasaDeviceDo -action @("on")
+		# simple way to reset the auto_off_at
+		# we want to have the plug shut off the light if this script dies unexpectedly
+		# but simply turning it on will not reset the auto-off time
+		KasaDeviceDo -action @("feature", "auto_off_enabled", "False")
+		KasaDeviceDo -action @("feature", "auto_off_enabled", "True")
+	} else {
+		KasaDeviceDo -action @("off")
+	}
     Start-Sleep -Milliseconds $sleepTimeMs
 }
+# turn off the light if we're exiting gracefully
+KasaDeviceDo -action "off"
